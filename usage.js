6d788d96ceb5a55779287a3f01423422031766d3
Sylvain Perron <sylvain.perron@protorisk.com>
2015-03-08 13:56:40 -0400

    Updated usage

--- usage.js
+++ usage.js
@@ -1,19 +1,25 @@
-var hose = require('cw-hose');
+var hose = require('./index');
 
 var src = new hose.Source({
-	LogGroup: '',
-	StartTime: new Date().getTime(),
-	EndTime: null,
-	LogStreamPrefix: '',
-	PollInterval: 1000
+	LogGroup: '/aws/lambda/MINUS-49dda359e9abcd4e180f73bd8ba7e2f9', // Mandatory
+	StartTime: new Date().getTime(), // Optional
+	EndTime: null, // Optional
+	LogStreamPrefix: '', // Optional
+	PollInterval: 1000, // Optional
+	aws: { region: 'us-west-2' } // Mandatory
 });
 
 src.on('logs', function(batch) {
-	
+	for(var i in batch) console.log('Log: ', batch[i].message.trim());
 });
 
 src.on('error', function(error) {
-	
+	console.log('Error: ', error);
 });
 
-src.Open();
+src.open();
+
+setTimeout(function() {
+	// close the hose after 1 min
+	src.close(); 
+}, 60 * 1000);
